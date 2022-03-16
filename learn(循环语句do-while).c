d9574103b4006db4e07c0c718d9762c50818cf39
Urmmine <101092356+Urmmine@users.noreply.github.com>
2022-03-16 16:17:28 +0800

    Add files via upload

---
+++ learn(循环语句do-while).c
@@ -0,0 +1,34 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <string.h>
+
+//do...while循环语句
+
+/*	do
+		循环语句;
+	while(表达式);
+*/
+
+
+//int main()
+//{
+//	int i = 1;
+//	do
+//	{
+//		if (i == 5)
+//			continue;
+//		//跳出本次循环，死循环i=5
+//		
+//			//break;
+//		//跳出整个循环
+//
+//		printf("%d ", i);
+//		i++;
+//	} while (i<=10);
+//
+//	return 0;
+//}
+
+
+
+
